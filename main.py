473dc904effe5f75857370e699cea1e02d43f175
Juan Peredo <jperedo@bolbeck.com>
2020-04-03 11:27:25 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,177 @@
+"""
+Tetris
+
+Tetris clone, from the arcade.academy tutorial
+"""
+import arcade
+import random
+import PIL
+
+# Set how many rows and columns we will have
+ROW_COUNT = 24
+COLUMN_COUNT = 10
+
+# This sets the WIDTH and HEIGHT of each grid location
+WIDTH = 30
+HEIGHT = 30
+
+# This sets the margin between each cell
+# and on the edges of the screen.
+MARGIN = 5
+
+# Do the math to figure out our screen dimensions
+SCREEN_WIDTH = (WIDTH + MARGIN) * COLUMN_COUNT + MARGIN
+SCREEN_HEIGHT = (HEIGHT + MARGIN) * ROW_COUNT + MARGIN
+SCREEN_TITLE = "Tetris"
+
+colors = [
+          (0,   0,   0),
+          (255, 0,   0),
+          (0,   150, 0),
+          (0,   0,   255),
+          (255, 120, 0),
+          (255, 255, 0),
+          (180, 0,   255),
+          (0,   220, 220)
+          ]
+
+# Define the shapes of the single parts
+tetris_shapes = [
+    [[1, 1, 1],
+     [0, 1, 0]],
+
+    [[0, 2, 2],
+     [2, 2, 0]],
+
+    [[3, 3, 0],
+     [0, 3, 3]],
+
+    [[4, 0, 0],
+     [4, 4, 4]],
+
+    [[0, 0, 5],
+     [5, 5, 5]],
+
+    [[6, 6, 6, 6]],
+
+    [[7, 7],
+     [7, 7]]
+]
+
+
+def create_textures():
+    """ Create a list of images for sprites based on the global colors. """
+    return ""
+    pass
+
+
+texture_list = create_textures()
+
+
+def rotate_clockwise(shape):
+    """ Rotates a matrix clockwise """
+    pass
+
+
+def check_collision(board, shape, offset):
+    """
+    See if the matrix stored in the shape will intersect anything
+    on the board based on the offset. Offset is an (x, y) coordinate.
+    """
+    pass
+
+
+def remove_row(board, row):
+    """ Remove a row from the board, add a blank row on top. """
+    pass
+
+
+def join_matrixes(matrix_1, matrix_2, matrix_2_offset):
+    """ Copy matrix 2 onto matrix 1 based on the passed in x, y offset coordinate """
+    pass
+
+
+def new_board():
+    """ Create a grid of 0's. Add 1's to the bottom for easier collision detection. """
+    pass
+
+
+class MyGame(arcade.Window):
+    """ Main application class. """
+
+    def __init__(self, width, height, title):
+        """ Set up the application. """
+
+        pass
+
+    def new_stone(self):
+        """
+        Randomly grab a new stone and set the stone location to the top.
+        If we immediately collide, then game-over.
+        """
+        pass
+
+    def setup(self):
+       pass
+
+    def drop(self):
+        """
+        Drop the stone down one place.
+        Check for collision.
+        If collided, then
+          join matrixes
+          Check for rows we can remove
+          Update sprite list with stones
+          Create a new stone
+        """
+        pass
+
+    def rotate_stone(self):
+        """ Rotate the stone, check collision. """
+        pass
+
+    def on_update(self, dt):
+        """ Update, drop stone if warrented """
+        pass
+
+    def move(self, delta_x):
+        """ Move the stone back and forth based on delta x. """
+        pass
+
+    def on_key_press(self, key, modifiers):
+        """
+        Handle user key presses
+        User goes left, move -1
+        User goes right, move 1
+        Rotate stone,
+        or drop down
+        """
+        pass
+
+    # noinspection PyMethodMayBeStatic
+    def draw_grid(self, grid, offset_x, offset_y):
+        """
+        Draw the grid. Used to draw the falling stones. The board is drawn
+        by the sprite list.
+        """
+        pass
+
+    def update_board(self):
+        """
+        Update the sprite list to reflect the contents of the 2d grid
+        """
+        pass
+
+    def on_draw(self):
+        """ Render the screen. """
+        pass
+
+def main():
+    """ Create the game window, setup, run """
+    my_game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    my_game.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
